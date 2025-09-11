260f6605e85b4f143aea8be5a9524f1ea1aba890
Isaac Noda <isaacnoda@gmail.com>
2025-09-11 17:18:45 -0600

    Simplify

--- js/fp_img.js
+++
@@ -1,8 +0,0 @@
-function fsImg() {
-  const imgs = document.querySelectorAll('.pics img');
-  imgs.forEach(img => {
-    img.addEventListener('click', function(e){
-      img.requestFullscreen();
-    })
-  });
-}
